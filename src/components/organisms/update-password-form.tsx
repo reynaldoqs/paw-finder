28e55c77f1e8653a685ae05adb00832bb88d0919
Reynaldo qs <reynaldoqs@gmail.com>
2025-12-15 11:16:30 -0400

    fixing link

--- src/components/organisms/update-password-form.tsx
+++ src/components/organisms/update-password-form.tsx
@@ -2,18 +2,19 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+
+import { createClient } from "@/lib/supabase/client";
+import { cn } from "@/lib/utils";
 import {
+  Button,
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { createClient } from "@/lib/supabase/client";
-import { cn } from "@/lib/utils";
+  Input,
+  Label,
+} from "../atoms";
 
 export function UpdatePasswordForm({
   className,
